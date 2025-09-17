4659f46b86252437c98b30fcf322f9fbf3d300df
Amada García <a01569104@tec.mx>
2025-09-17 15:22:54 -0600

    min fix

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Route handlers for /hello endpoints
+
 app.get('/hello/get', (req, res) => {
     res.send('Hello, World!');
 });
@@ -23,9 +22,6 @@ app.delete('/hello/:id', (req, res) => {
     res.send(`Deleted item with id: ${id}`);
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 module.exports = app;
+
